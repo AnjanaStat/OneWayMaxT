e16f01b65a16e66ddf25234363ba77c22acaf64e
AnjanaStat <107476002+AnjanaStat@users.noreply.github.com>
2022-07-17 19:42:32 +0530

    Initial commit

---
+++ R/OneWayMaxT.R
@@ -0,0 +1,11 @@
+#' Find test statistic value and corresponding critical value from a given data set and level of significance
+#'
+#' This package finds test statistic value and
+#' critical value of Max-T test for testing equality of means aginst
+#' ordered alternatives in one-way ANOVA model with heteroscedasticity.
+#'
+#' @docType package
+#'
+#' @author Anjana Mondal \email{mondal.anjana25@gmail.com}
+#'
+#' @name OneWayMaxT
---
+++ README.md
@@ -0,0 +1,2 @@
+# OneWayMaxT
+ This package finds test statistic value and critical value for testing equality of treatment effects against ordered alternatives in one-way ANOVA model with heteroscedasticity
